a0a009339a483628616f052413dc77eab28d9cd4
HyunahKwon <hakwon9439@gmail.com>
2018-10-30 00:04:49 +0900

    2018/10/29 사칙연산 > 2839 설탕 배달

--- ByMyself/2_calculation/2839_sugar_delivery.cpp
+++ ByMyself/2_calculation/2839_sugar_delivery.cpp
@@ -1,37 +1,37 @@
 /* 정확하게 N킬로 배달
 	봉지는 3kg, 5kg △ 최대한 적은 봉지
+
+테스트
+3: 1
+4: -1
+5: 1
+6: 2
+7: -1
+8: 2
+9: 3
+10: 2
+11: 3
+12: 4
 */
 #include <iostream>
 using namespace std;
 
 int main() {
 	int n;
-	int bag = 0;
+	int bag = -1;
 	cin >> n;
 
-	while (n > 0) {
-		if (n == 3 || n == 5) {
-			bag++;
+	for (int i = n / 5 * 5; i >= 0; i -= 5) {
+		if ((n - i) % 5 == 0) {					// 5의 배수
+			bag = i / 5;
 			break;
 		}
-		else if (n == 1 || n == 2 || n == 4) {
-			bag = -1;
+		else if ((n - i) % 3 == 0) {			// 3의 배수
+			bag = (n - i) / 3 + i / 5;
 			break;
 		}
-		else {
-			bag++;
-			n = n - 5;
-			if (n % 3 == 0) {	// 3의 배수
-				bag = bag + (n / 3);
-				break;
-			}
-			else if (n % 5 == 0) {	// 5의 배수
-				bag = bag + (n / 5);
-				break;
-			}
-		}
+
 	}
 	cout << bag;
-
 	return 0;
 }
