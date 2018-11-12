aec0516f065aba06509b924469cc35b015ffdacd
HyunahKwon <hakwon9439@gmail.com>
2018-11-12 23:34:12 +0900

    2018/11/12 브루트 포스 > 7568 덩치

--- ByMyself/22_brute_force/7568_dungchi.cpp
+++ ByMyself/22_brute_force/7568_dungchi.cpp
@@ -1,54 +1,31 @@
-/* 거꾸로 했을 때 두 수 중 더 큰 수 출력 */
+/* 덩치 순서 */
 #include <iostream>
 using namespace std;
 
 int main() {
-	int n, info[50][3];	// 몸무게, 키, 덩치 순서
-	int weight[50];
+	int n, info[50][2];	// 몸무게, 키
+	int ans[50];		// 덩치 순서
 
-	//// initialize
-	for (int i = 0; i < 50; i++) {
-	//	for (int j = 0; j < 2; j++) {
-	//		info[i][j] = 0;
-	//	}
-		info[i][2] = 50;
-	//	weight[i] = -1;
-	}
-
-	//input
+	//input & initialize
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> info[i][0] >> info[i][1];	// x, y
+        ans[i] = 1;
 	}
 
-	// compare1: 몸무게로 순위 정하기
-	for (int j = 1; j <= n; j++) {
-		int max = 0, temp = 0;
-		for (int i = 0; i < n; i++) {
-			if (info[i][0] > max && info[i][2] == 50) {
-				max = info[i][0];
-				temp = i;
-			}
-		}
-		info[temp][2] = j;
-		weight[j - 1] = temp;
-	}
-
-	// compare2: 순위로 접근해서 키 비교
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			// 몸무게는 작지만 키가 크면, 덩치 등수는 같아짐
-			if (info[weight[i]][1] < info[j][1] && j != weight[i] && info[j][2] > i) { 
-				info[j][2] = info[weight[i]][2];
+	// compare: 몸무게랑 키가 나보다 큰 사람이 있냐?
+	for (int j = 0; j < n; j++) {			// j: 나
+		for (int i = 0; i < n; i++) {		// i: 남... 나랑 나를 비교해도 등수 변화 없음
+			if (info[i][0] > info[j][0] && info[i][1] > info[j][1]) {
+				ans[j]++;					// 내 등수는 뒤로 밀림
 			}
 		}
 	}
 
 	// output
 	for (int i = 0; i < n; i++) {
-		cout << info[i][2] << " ";
+		cout << ans[i] << " ";
 	}
-	
+
 	return 0;
 }
-
