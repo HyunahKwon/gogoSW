b1576fbbb74a778aca431f7094a86f654f09dd7e
HyunahKwon <hakwon9439@gmail.com>
2018-11-27 00:34:32 +0900

    2018/11/27 소수구하기 > 4948 베르트랑 공준

--- ByMyself/10_prime#/4948_bertrand.cpp
+++ ByMyself/10_prime#/4948_bertrand.cpp
@@ -5,31 +5,33 @@ using namespace std;
 
 int main() {
 	int n, prime[MAX];
+	int cnt = 0;
 
+	// 에라토스테네스의 체
+	for (int i = 0; i < MAX; i++) {
+		if (i == 1) prime[i] = 0;			// 1은 소수가 아니다!
+		else
+			prime[i] = i;					// 자기 자신 넣기
+	}
+	for (int i = 2; i < MAX; i++) {
+		for (int j = 2; j < MAX && i*j < MAX; j++) {
+			prime[i*j] = 0;
+		}
+	}
+
+	// testcase
 	while (1) {
 		cin >> n;
 		int cnt = 0;
 		if (n == 0) break;
-		for (int i = 0; i <= 2 * n; i++) {
-			if (i == 1) prime[i] = 0;			// 1은 소수가 아니다!
-			else
-				prime[i] = i;					// 자기 자신 넣기
-		}
-
-		// prime number?
-		for (int i = 2; i <= 2 * n; i++) {
-			for (int j = 2; j <= 2 * n && i*j < MAX; j++) {
-				prime[i*j] = 0;
-			}
-		}
 
 		// output
 		for (int i = n + 1; i <= 2 * n; i++) {
-			if (prime[i] != 0)
+			if (prime[i] != 0) {
 				cnt++;
+			}
 		}
 		cout << cnt << endl;
 	}
-
 	return 0;
 }
